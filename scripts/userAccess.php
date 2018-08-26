9bfb1478aec23b070fb8729dbb0bede558ff8793
edsiii <edsiii@fuse.net>
2018-08-26 09:49:03 -0400

    Changes ref deployment

--- scripts/userAccess.php
+++ scripts/userAccess.php
@@ -16,9 +16,10 @@ if ( $cid == $appCid ) {
 	$SQL_query = "SELECT idx, userID, franID, FirstName, LastName, email, phone1, accessLevel FROM users WHERE loginName = '".$loginName."' AND pwd = '".$pwd."' AND franID = '".$franID."' AND active = 1 ;"; 	
 
 	//$SQL_query = "SELECT idx, userID, franID, FirstName, LastName, email, phone1, accessLevel FROM users WHERE loginName = 'joel' AND pwd = 'none' AND franID = 'GD00KS' AND active = 1 ;";
-	
-	$DB_link = mysqli_connect($host, $user, $pass, $database) or die("Could not connect to host.");
+	//$DB_link = mysqli_connect($host, $user, $pass, $database) or die("Could not connect to host.");
 	$connection = mysqli_connect($host, $user, $pass, $database) or die ("Could not find or access the database.");
+	//$connection = mysqli_connect("mysql.greatdaymoving.com", "gd6user", "gd6user0532", "gd6testdb") or die ("Could not find or access the database.");
+	
   // Query to run
   $query = mysqli_query($connection,$SQL_query);	
 	// Create empty array to hold query results
@@ -28,16 +29,11 @@ if ( $cid == $appCid ) {
   
 	  // Loop through query and push results into $someArray;
 	  while ($row = mysqli_fetch_assoc($query)) {
-			//echo $query->num_rows;
 		array_push($someArray, [
-		  'idx'   => $row['idx'],
-		  // add columns here
-		  
+		  'idx'   => $row['idx'],		  
 		  'userID' 		=> $row['userID'],
 		  'FirstName' 	=> $row['FirstName'],
 		  'LastName' 	=> $row['LastName'],
-
-		  // end add columns
 		  'status'   	=> 'OK',
 		  'franID' 		=> $row['franID'],
 		  'email'		=> $row['email'],
